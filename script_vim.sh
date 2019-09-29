45cc9922523e4b9e9b74262df2ae5cd7340841b7
ChevalierSoft <root@vps597414.ovh.net>
2019-09-30 01:43:56 +0200

    First commit. It shoud work fine.

---
+++ script_vim.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+vim= ~/.vim
+vimrc=~/.vimrc
+colors=~/.vim/colors
+n_scheme=molokai.vim
+a_scheme=$colors/$n_scheme
+
+if [ -f $vimrc ]
+then
+	date=$(date '+%d-%m-%y_%H:%M:%S')
+	new_vimrc=".vimrc-$date"
+	echo ".vimrc archived as $new_vimrc"
+	#echo ~/$new_vimrc
+
+#	mv $vimrc ~/$new_vimrc
+fi
+
+#cp ./vimrc $vimrc
+
+if [ ! -d $vim ]
+then
+	mkdir $vim
+fi
+
+if [ ! -d $colors ]
+then
+	mkdir $colors
+fi
+
+if [ ! -f $a_scheme ]
+then
+	echo "scheme copied"
+	cp ./colors/$n_scheme $a_scheme
+fi
+
+
---
+++ vimrc
@@ -0,0 +1,7 @@
+syntax on
+set mouse=a
+set tabstop=4
+set shiftwidth=4
+set nu
+colorscheme molokai
+
