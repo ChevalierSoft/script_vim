6551720883aa93940a1a42b10e61529673e39b1c
ChevalierSoft <ibanarashi@outlook.fr>
2023-02-01 15:31:50 +0100

    added new scripts to the main setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
-bash script_tmux.sh
-bash script_zsh.sh
-bash script_bashrc.sh
-bash script_vim.sh
+#!/bin/bash
+
+bash ./script_tmux.sh
+bash ./script_zsh.sh
+bash ./script_bashrc.sh
+bash ./script_vim.sh
+bash ./nvim-install.sh
+bash ./nvchad-install.sh
+bash ./utils-install.sh
