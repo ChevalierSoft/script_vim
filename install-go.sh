538619ae3674190bfcec119451752c01fe418793
daris <daitatmane@freebox.fr>
2024-04-17 18:32:24 +0200

    getting go 1.22.2 tar.gz

--- install-go.sh
+++ install-go.sh
@@ -1,3 +1,5 @@
+wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+rm -f go1.22.2.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 go version
