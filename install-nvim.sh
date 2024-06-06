ace0c5c1006295497b977a2215ac70bfba8c32dd
ChevalierSoft <ibanarashi@outlook.fr>
2024-06-06 14:26:17 +0200

    Update install-nvim.sh

--- install-nvim.sh
+++ install-nvim.sh
@@ -2,4 +2,4 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
-echo "export PATH=\"$PATH:/opt/nvim-linux64/bin\" >> ~/.profile"
+echo 'export PATH=$PATH:/opt/nvim-linux64/bin' >> ~/.profile
