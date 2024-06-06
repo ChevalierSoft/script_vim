37b85ac0c63e04857abb9487f21feff6ab5ceb27
ChevalierSoft <ibanarashi@outlook.fr>
2024-06-06 14:03:47 +0200

    Update install-nvim.sh

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,1 +1,4 @@
-sudo dpkg -i ./nvim-linux64.deb
+#!/bin/bash
+wget https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-linux64.tar.gz
+tar xzf nvim-linux64.tar.gz
+mv ./nvim-linux64/bin/nvim /usr/local/bin/
