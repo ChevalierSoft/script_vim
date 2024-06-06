51775da9a259dc644f80cb684ce9112d5943b3e2
ChevalierSoft <ibanarashi@outlook.fr>
2024-06-06 14:05:45 +0200

    Update install-go.sh

--- install-go.sh
+++ install-go.sh
@@ -1,5 +1,5 @@
-wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz
-rm -f go1.22.2.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
+rm -f go1.22.4.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin:~/go/bin' >> ~/.profile
 go version
