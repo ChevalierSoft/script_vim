5a4343f6a572abc059e3ce6b389ce58f3b9f28ff
ChevalierSoft <ibanarashi@outlook.fr>
2025-06-05 23:23:32 +0200

    update zshrc conf

--- script_zsh.sh
+++ script_zsh.sh
@@ -6,7 +6,7 @@ if [ -f $zshrc ]
 then
 	date=$(date '+%d-%m-%y_%H:%M:%S')
 	new_zshrc=".zshrc-$date"
-	echo ".zshrc archived as $new_vimrc"
+	echo ".zshrc archived as $new_zshrc"
 	mv $zshrc ~/$new_zshrc
 fi
 
